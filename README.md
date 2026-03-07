68e45c3ae344c4f0b2d4b94c00b97c84eb365f5b
michael <michael@DESKTOP-974M4MO.localdomain>
2026-03-07 21:09:37 +0100

    Added multithreaded scanning and banner grabbing

---
+++ README.md
@@ -0,0 +1,12 @@
+A multithreaded port scanner written in python
+
+Features:
++ Multithreaded Scanning
++ Banner Grabbing
++ Service Detection
+
+Built for cybersecurity learning.
+
+Usage:
+python3 scanner.py
+
--- scanner.py
+++ scanner.py
@@ -1,16 +1,59 @@
 import socket
+import threading
+from queue import Queue
 
-target = input("Enter target IP: ")
+target = input("Enter target IP or domain: ")
+
+target = target.replace("http://", "").replace("https://", "").split("/")[0]
 
 print(f"\nScanning {target}...\n")
 
+queue = Queue()
+
+
+
+def grab_banner(s):
+    try:
+        banner = s.recv(1024).decode().strip()
+        return banner
+    except:
+        return "Unknown Service"
+
+
+def scan_port(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            banner = grab_banner(s)
+            print(f"port {port} OPEN - {banner}")
+
+        s.close()
+
+    except:
+         pass
+
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        scan_port(port)
+        queue.task_done()
+
+
 for port in range(1, 1025):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+    queue.put(port)
 
-    result = s.connect_ex((target, port))
 
-    if result == 0:
-        print(f"port {port} is OPEN")
+for t in range(100):
+    thread = threading.Thread(target=worker)
+    thread.daemon = True
+    thread.start()
 
-    s.close()
+
+queue.join()
+
+print("\nScan completed!")
